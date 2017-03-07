42bcd784cbe9e150ef94234e340c941e888c6e53
Paul Sanford <me@paulsanford.net>
2017-03-07 01:59:08 -0500

    Move input handling to game

--- game.go
+++ game.go
@@ -4,6 +4,7 @@ type Game struct {
 	term   Terminal
 	m      Map
 	player Player
+	input  Input
 }
 
 func (g Game) init() {
@@ -18,4 +19,26 @@ func (g Game) draw() {
 	g.m.draw(g.term)
 	g.player.draw(g.term)
 	g.term.flush()
+}
+
+func (game Game) loop() {
+	for {
+		game.draw()
+		ev := game.input.getchar()
+		if ev == 'q' {
+			break
+		}
+		if ev == 'k' {
+			game.player.loc.y -= 1
+		}
+		if ev == 'j' {
+			game.player.loc.y += 1
+		}
+		if ev == 'h' {
+			game.player.loc.x -= 1
+		}
+		if ev == 'l' {
+			game.player.loc.x += 1
+		}
+	}
 }
