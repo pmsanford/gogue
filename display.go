bd5435cdf1f5ac30d58d3bb09076dea3bb3fd7d2
Paul Sanford <me@paulsanford.net>
2017-03-07 01:59:19 -0500

    Abstract colors

--- display.go
+++ display.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	//"fmt"
 	"github.com/nsf/termbox-go"
 )
 
 type Drawable interface {
 	draw(term Terminal)
 }
+
+type Color uint16
+
+const (
+	DefaultColor Color = iota
+	Black
+	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
+)
 
 type Terminal interface {
 	init()
@@ -18,23 +31,27 @@ type Terminal interface {
 }
 
 type tboxterm struct {
-	fg, bg termbox.Attribute
+	fg, bg Color
 }
 
 func (t tboxterm) draw_char(x, y int, c rune) {
-	termbox.SetCell(x, y, c, t.fg, t.bg)
+	t.draw_char_ex(x, y, c, t.fg, t.bg)
+}
+
+func (t tboxterm) draw_char_ex(x, y int, c rune, fg, bg Color) {
+	termbox.SetCell(x, y, c, termbox.Attribute(fg), termbox.Attribute(bg))
 }
 
 func (t tboxterm) draw_str(x, y int, s string) {
 	for _, c := range s {
 		t.draw_char(x, y, c)
 		x++
-	}	
+	}
 }
 
 func (t tboxterm) init() {
 	termbox.Init()
-	termbox.Clear(termbox.ColorWhite, termbox.ColorBlack);
+	termbox.Clear(termbox.ColorWhite, termbox.ColorBlack)
 }
 
 func (t tboxterm) flush() {
@@ -46,5 +63,5 @@ func (t tboxterm) close() {
 }
 
 func GetTerminal() Terminal {
-	return tboxterm {fg: termbox.ColorWhite, bg: termbox.ColorBlack }
+	return tboxterm{fg: White, bg: Black}
 }
--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 func main() {
-	game := Game{term: GetTerminal(), m: CreateMap(), player: Player{hp: 100, loc: Location{x: 5, y: 5}, char: '@'}}
+	game := Game{term: GetTerminal(), m: CreateMap(), player: Player{hp: 100, loc: Location{x: 5, y: 5}, char: '@'}, input: GetInput()}
 	game.init()
 	defer game.end()
 
 	game.draw()
-
-	input := GetInput()
-
-	for {
-		game.draw()
-		ev := input.getchar()
-		if ev == 'q' {
-			break
-		}
-		if ev == 'k' {
-			game.player.loc.y -= 1
-		}
-		if ev == 'j' {
-			game.player.loc.y += 1
-		}
-		if ev == 'h' {
-			game.player.loc.x -= 1
-		}
-		if ev == 'l' {
-			game.player.loc.x += 1
-		}
-	}
+	game.loop()
 }
